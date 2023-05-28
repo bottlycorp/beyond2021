150b57f018b1dae2aceeeb3392069cc2ab1ea186
Gaëtan H <steellgold0@gmail.com>
2023-05-28 23:46:49 +0200

    feat(beyond.ts): add support for multiple Google Search API keys to shift rate limit

--- src/beyond.ts
+++ src/beyond.ts
@@ -1,16 +1,7 @@
-import { compile } from "html-to-text";
-import { encode } from "gpt-3-encoder";
 import { Configuration as OpenAIConfiguration, OpenAIApi } from "openai";
-import { Configuration, SafeSearch, SearchResponse, SearchResult, SearchResults } from "./beyond.types";
+import { Configuration, SearchResponse, SearchResult } from "./beyond.types";
 
-const GOOGLE_REQUEST = "https://www.googleapis.com/customsearch/v1?key={1}&cx={2}&q={3}&safe={4}";
-
-const convert = compile({
-  preserveNewlines: false,
-  wordwrap: false,
-  baseElements: { selectors: ["main"] },
-  selectors: [{ selector: "a", options: { ignoreHref: true } }]
-});
+const GOOGLE_REQUEST = "https://www.googleapis.com/customsearch/v1?key={apiKey}&cx={searchEngineId}&q={query}&num={num}";
 
 export class DataBeyond {
 
@@ -31,66 +22,78 @@ export class DataBeyond {
     }));
   }
 
-  public search = async(prompt: string, safeSearch: SafeSearch = "active"): Promise<SearchResponse> => {
-    const googleResponse = await fetch(GOOGLE_REQUEST
-      .replace("{1}", this.config.GOOGLE_SEARCH_API_KEY)
-      .replace("{2}", this.config.GOOGLE_SEARCH_ENGINE_ID)
-      .replace("{3}", prompt)
-      .replace("{4}", safeSearch));
+  private async find(query: string, limit = 1): Promise<Response> {
+    const data = await fetch(GOOGLE_REQUEST
+      .replace("{apiKey}", this.config.GOOGLE_SEARCH_API_KEY)
+      .replace("{searchEngineId}", this.config.GOOGLE_SEARCH_ENGINE_ID)
+      .replace("{query}", encodeURIComponent(query))
+      .replace("{num}", limit.toString()));
 
-    const [context, urlReference] = (await this.getTextOfSearchResults(await googleResponse.json()));
+    if (data.status !== 429) return data;
 
-    const res = await this.openai.createChatCompletion({
-      messages: [
-        { role: "assistant", content: context },
-        { role: "user", content: `With the information in the assistant's last message, answer this in the same language: ${prompt}` }
-      ],
-      model: "gpt-3.5-turbo",
-      max_tokens: 100
-    });
+    console.error("This key is rate limited by Google, trying another key...");
+    let response: Response;
 
-    if (!res.data) return { content: "No response from OpenAI" };
-    const response = res.data.choices[0].message?.content;
-    if (!response) return { content: "No response from OpenAI" };
+    if (this.config.MULTIPLE_SEARCH_API_KEYS && this.config.MULTIPLE_SEARCH_API_KEYS.length > 0) {
+      let keyIndex = 0;
+      while (keyIndex < this.config.MULTIPLE_SEARCH_API_KEYS.length) {
+        try {
+          response = await fetch(GOOGLE_REQUEST
+            .replace("{apiKey}", this.config.MULTIPLE_SEARCH_API_KEYS[keyIndex])
+            .replace("{searchEngineId}", this.config.GOOGLE_SEARCH_ENGINE_ID)
+            .replace("{query}", encodeURIComponent(query))
+            .replace("{num}", limit.toString()));
 
-    if (safeSearch !== "off") {
-      const moderation = await this.openai.createModeration({
-        input: urlReference + "\n" + response,
-        model: "text-moderation-latest"
+          if (response.status === 429) {
+            console.error("This key is also rate limited by Google, trying another key...");
+            keyIndex += 1;
+          } else {
+            console.log("Successfully found a key that is not rate limited by Google.");
+            return response;
+          }
+        } catch {
+          keyIndex += 1;
+        }
+      }
+    }
+
+    return data;
+  }
+
+  public async search(query: string, limit = 1): Promise<SearchResponse> {
+    let context = "";
+    try {
+      const response = await this.find(query, limit);
+      if (response.status !== 200) return { content: "This request failed, please try again later.", url: null };
+
+      const data = await response.json();
+      const items: SearchResult[] = data.items;
+
+      if (!items || items.length === 0) return { content: "No results found.", url: null };
+
+      context = items.map(item => item.snippet).join("\n");
+
+      const result = await this.openai.createChatCompletion({
+        messages: [
+          { content: context, role: "assistant" },
+          { content: `With the information in the assistant's last message, answer this in the same language: ${query}`, role: "user" }
+        ],
+        model: "gpt-3.5-turbo"
       });
 
-      if (!moderation.data) return { content: "No response from OpenAI" };
-      const flagged = moderation.data.results[0].flagged;
-      if (flagged) return { content: "This response was flagged as inappropriate" };
+      if (!result || !result.data || !result.data.choices) return { content: "No response from OpenAI", url: null };
+
+      const responseText = result.data.choices[0].message?.content;
+      if (!responseText) return { content: "No response from OpenAI", url: null };
+
+      return {
+        content: responseText,
+        url: items[0].link,
+        urls: items.map(item => item.link)
+      };
+    } catch (error: any) {
+      return { content: "An error occurred while searching Google.", url: null };
     }
-
-    return { content: response, url: urlReference };
-  };
-
-  private getTextOfSearchResults = async(results: SearchResults): Promise<[string, string]> => {
-    let urlReference = "";
-
-    if (!results.items || results.items.length === 0) return ["", urlReference];
-
-    let context = results.items.reduce(
-      (allPages: string, currentPage: SearchResult) => `${allPages} ${currentPage.snippet.replaceAll("...", " ")}`, ""
-    );
-
-    for (let i = 0; i < results.items.length && i < 5; i++) {
-      const urlToCheck = results.items[i].link;
-      const response = await fetch(urlToCheck);
-      const fullText = convert(await response.text()).replaceAll("\n", " ").trim();
-
-      context = fullText + context;
-      urlReference = urlToCheck;
-      break;
-    }
-
-    const maxPromptTokenLength = 3000;
-    const encoded = encode(context);
-
-    if (encoded.length > maxPromptTokenLength) context = context.slice(0, maxPromptTokenLength);
-    return [context, urlReference];
-  };
+  }
 
 }
--- src/beyond.types.ts
+++ src/beyond.types.ts
@@ -6,13 +6,23 @@ export type Configuration = {
   OPENAI_API_KEY: string;
   /**
    * Optional, only needed for users with multiple organizations (default: null)
+   * @see https://platform.openai.com/account/org-settings
    */
   OPENAI_ORGANIZATION_ID?: string;
   /**
    * Google Search API key
+   * You can send 100 requests per day for free. (per key)
+   * @see https://developers.google.com/custom-search/v1/overview#pricing
    * @see https://developers.google.com/custom-search/v1/introduction
    */
   GOOGLE_SEARCH_API_KEY: string;
+  /**
+   * Since we can only make 100 requests per day (per key) without charge, we define several keys here to randomly
+   * select each request in order to shift this limit further and further out.
+   * If a key is rate limited, the next key will be used. If all keys are rate limited, the request will fail.
+   * @see https://developers.google.com/custom-search/v1/overview#pricing
+   */
+  MULTIPLE_SEARCH_API_KEYS?: string[];
   /**
    * Google Search Engine ID
    * @see https://programmablesearchengine.google.com/controlpanel/all
@@ -20,21 +30,16 @@ export type Configuration = {
   GOOGLE_SEARCH_ENGINE_ID: string;
 }
 
-export type SearchResults = {
-  items: {
-    snippet: string;
-    link: string;
-  }[];
-}
-
-export type SearchResult = {
-  snippet: string;
-  link: string;
-}
-
 export type SearchResponse = {
   content: string;
   url?: string | null;
+  urls?: string[] | null;
+}
+
+export type SearchResult = {
+  title: string;
+  link: string;
+  snippet: string;
 }
 
 /**
